472855e8bbd8eb3133b43bf3a2d6056900c73350
unknown <saied2421998@gmail.com>
2022-05-30 19:56:17 +0200

    add Item class & setup simple app.rb with main.rb

---
+++ item.rb
@@ -0,0 +1,32 @@
+# item classs
+class Item
+	def initialize(date)
+		@id = Random.rand(1..1000)
+		@genre = nil
+		@author = nil
+		@label = nil
+		@publish_date = date
+		@archived = move_to_archive(@publish_date)
+	end
+
+	def add_genre
+			
+	end
+    
+    def add_author
+
+    end
+
+    def add_label
+
+    end
+
+    def can_be_archived?(date)
+     (Date.today - Date.parse(date)).to_i > 10
+    end	
+
+    def move_to_archive(date)
+     can_be_archived?(date)
+    end	
+
+end	
