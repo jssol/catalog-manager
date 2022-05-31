04b378ede6e5b362e6076fa03986c1fc123ef0d8
Jonathan Sivahera <jsivahera@icloud.com>
2022-05-31 23:59:46 +0200

    Add book class table

--- schema.sql
+++ schema.sql
@@ -1,4 +1,23 @@
 /* Create Label class table */
-CREATE TABLE IF NOT EXISTS label (id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY, title TEXT, color TEXT);
+CREATE TABLE IF NOT EXISTS label (
+	id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	title TEXT,
+	color TEXT,
+	items JSON []
+);
 
-
+/* Create Book class table */
+CREATE TABLE IF NOT EXISTS book (
+  id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  genre JSON,
+  author JSON,
+  source JSON,
+  label JSON,
+  publish_date DATE,
+  archived bool,
+  publisher TEXT,
+  cover_state TEXT,
+  label_id INT NOT NULL,
+  FOREIGN KEY (label_id)
+      REFERENCES label (id),
+);
