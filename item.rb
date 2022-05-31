f4199386d3fb846286c0f8f1862776236b4a79e9
unknown <saied2421998@gmail.com>
2022-05-31 15:27:41 +0200

    Finish game class unit tests

--- item.rb
+++ item.rb
@@ -2,7 +2,7 @@ require 'date'
 require_relative './author'
 # item classs
 class Item
-  attr_reader :genre, :label, :author, :date
+  attr_reader :id, :archived, :genre, :label, :author, :publish_date
 
   def initialize(date)
     @id = Random.rand(1..1000)
@@ -16,12 +16,14 @@ class Item
   def add_genre; end
 
   def add_author(author)
-    author.is_a?(Author) && @author.nil? && (
+    !author.is_a?(Author) && (
+      puts 'Wrong paramater. This method only accepts instances of Author class'
+      return
+    )  
+    @author.nil? && (
       @author = author
       author.add_item(self)
     )
-    puts 'there is an author already. you can\'t change it' unless @author.nil?
-    puts 'you passed wrong pramater. Rhis method only accept instances of Author class' unless author.is_a?(Author)
   end
 
   def add_label; end
