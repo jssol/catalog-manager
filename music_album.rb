06452a469c54498729bb0dedb7fdd0613552daee
Abenezer-Tilahun <abenezermesafint2128@gmail.com>
2022-05-31 13:17:23 +0300

    Add MusicAlbum and required functions

---
+++ music_album.rb
@@ -0,0 +1,14 @@
+class MusicAlbum < Item
+  attr_accessor :on_spotify
+
+  def initialize(publish_date, on_spotify: false)
+    super(publish_date)
+    @on_spotify = on_spotify
+  end
+
+  private
+
+  def can_be_archived?
+    super && @on_spotify
+  end
+end
