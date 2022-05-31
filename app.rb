26c10f8bb589dacc7d44cd9501d7b3094a7624dc
Jonathan Sivahera <jsivahera@icloud.com>
2022-05-31 19:43:31 +0200

    Add add_book && add_label method

--- app.rb
+++ app.rb
@@ -1,20 +1,43 @@
-# app class
+require_relative './book'
+require_relative './label'
+
 require 'io/console'
 class App
+  attr_reader :book_list, :label_list
+
+  def initialize
+    @book_list = []
+    @label_list = []
+  end
+
   def display_options
-    puts 'Welcome to your catalog manager\n\n'
-    puts 'Choose a catagory'
+    puts 'Welcome to your catalog manager!'
+    puts ''
+    puts 'Choose an option on the list:'
+    puts '-----------------------------'
     puts '1 - Books'
     puts '2 - Music albums'
     puts '3 - Games'
+    puts '4 - Save and exit'
     puts ''
-    puts 'You can\'t choose now. App is still in development state.\n\n'
-    print 'Press any key to close the app... '
+  end
+
+  def add_book(date, publisher, cover_state)
+    book = Book.new(date, publisher, cover_state)
+    @book_list << book
+  end
+
+  def add_label(item, title, color)
+    label = Label.new(title, color)
+    label.add_item(item)
+    @label_list << label
+  end
+
+  def display_books
+    @book_list.each { |_book| puts %(Publisher: "#{book.publisher}") }
   end
 
   def run
     display_options
-    $stdin.getch
-    puts ''
   end
 end
