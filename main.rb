712a67b73a0ee28c75548f37f4049cf53acc0ea1
Jonathan Sivahera <jsivahera@icloud.com>
2022-05-31 22:38:28 +0200

    Add book menu interactions

--- main.rb
+++ main.rb
@@ -2,7 +2,6 @@ require_relative './app'
 
 def display_book_console(app)
   app.menu = 'book'
-  puts 'book_console'
 end
 
 def display_music_console(app)
@@ -23,11 +22,45 @@ def add_book(app)
   print 'Cover state: '
   cover_state = gets.chomp
   app.add_book(date, publisher, cover_state)
+  puts ''
   puts 'Book created successfully!'
 end
 
-def display_books(app)
+def add_label_to_book(app)
+  if app.book_list.empty?
+    puts 'No books are available to be labelled!'
+    return
+  end
+  puts 'Choose the book to label by number:'
+  puts '-----------------------------------'
+  puts ''
   app.display_books
+  puts ''
+  print '--> '
+  book_index = gets.chomp.to_i - 1
+  book = app.book_list[book_index]
+  puts ''
+  print 'Enter the label title: '
+  title = gets.chomp
+  print 'Enter the label color: '
+  color = gets.chomp
+  app.add_label(book, title, color)
+end
+
+def display_books(app)
+  if app.book_list.empty?
+    puts 'No books are available!'
+    return
+  end
+  app.display_books
+end
+
+def display_labels(app)
+  if app.label_list.empty?
+    puts 'No labels are available!'
+    return
+  end
+  app.display_labels
 end
 
 def main_menu(app)
@@ -52,12 +85,12 @@ def take_action(app)
     ]
     '123'.include?(decision) && methods[decision.to_i - 1].call(app)
   when 'book'
-    puts 'Please choose one of the options on the list' unless '1234'.include?(decision)
-    decision == '4' && exit_app
+    puts 'Please choose one of the options on the list' unless '123456'.include?(decision)
+    decision == '6' && exit_app
     methods = [
-      method(:add_book), method(:display_books), method(:main_menu)
+      method(:add_book), method(:add_label_to_book), method(:display_books), method(:display_labels), method(:main_menu)
     ]
-    '123'.include?(decision) && methods[decision.to_i - 1].call(app)
+    '12345'.include?(decision) && methods[decision.to_i - 1].call(app)
   else
     puts 'Consider adding items to your catalog!'
     exit_app
