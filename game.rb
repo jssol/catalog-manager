777a7a88b9eb84deda42c93fcfcdeacb044658e6
Jonathan Sivahera <jsivahera@icloud.com>
2022-06-02 23:33:19 +0200

    Use a single variable name

--- game.rb
+++ game.rb
@@ -3,18 +3,18 @@ require_relative './item'
 # game class
 
 class Game < Item
-  attr_reader :multiplayer, :last_played_at
+  attr_reader :multiplayer, :last_played
 
   def initialize(date, multiplayer, last_played)
     super(date)
     @multiplayer = multiplayer
-    @last_played_at = last_played
+    @last_played = last_played
   end
 
   private
 
   def can_be_archived?
-    return true if super() && (Date.today - Date.parse(@last_played_at)).to_i > 2 * 365
+    return true if super() && (Date.today - Date.parse(@last_played)).to_i > 2 * 365
 
     false
   end
