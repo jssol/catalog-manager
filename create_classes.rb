92c2968f1c712102b950297dfd0d913cf502f12a
Jonathan Sivahera <jsivahera@icloud.com>
2022-06-03 00:43:23 +0200

    Improve UX && Fix linters

--- create_classes.rb
+++ create_classes.rb
@@ -6,14 +6,13 @@ require_relative './label'
 require_relative './game'
 require_relative './file_manager'
 
-$file_manager = FileManager.new
-
 class CreateClasses
   attr_reader :item_list, :label_list, :genre_list, :author_list
   attr_accessor :menu
 
   def initialize
     @menu = 'main'
+    @file_manager = FileManager.new
     @item_list = { book: [], musicalbum: [], game: [] }
     @label_list = { book: [], musicalbum: [], game: [] }
     @genre_list = { book: [], musicalbum: [], game: [] }
@@ -27,7 +26,7 @@ class CreateClasses
       # instance_variable_get(var).each do |obj|
       #   file.push({ ref: obj, value: to_hash(obj) })
       # end
-      $file_manager.save_file("./data/#{file_name}.json", file) if var.size.positive?
+      @file_manager.save_file("./data/#{file_name}.json", file) if var.size.positive?
     end
   end
 
@@ -62,20 +61,23 @@ class CreateClasses
   end
 
   def create_new_label(item, label_decision)
-    puts ''
-    if label_decision.downcase.strip == 'new'
+    label_decision = label_decision.downcase.strip
+    !item[:label].nil? && (
+      puts 'This item already has a label'
+      return
+    )
+    if label_decision == 'new'
       print 'Enter the label title: '
       title = gets.chomp
       print 'Enter the label color: '
       color = gets.chomp
       add_label(item, title, color)
-    elsif label_decision.to_i.is_a? Integer
+    elsif label_decision.to_i.is_a?(Integer)
       label_index = label_decision.to_i - 1
       label = @label_list[@menu.to_s.to_sym][label_index][:ref]
       label.add_item(item)
     else
-      puts ''
-      puts 'Invalid input! Try again!'
+      puts "\n\nInvalid input! Try again!"
       create_new_label(app, item, label_decision)
     end
     puts 'The label has been added successfully!'
@@ -88,18 +90,21 @@ class CreateClasses
   end
 
   def create_new_genre(item, genre_decision)
-    puts ''
-    if genre_decision.downcase.strip == 'new'
+    genre_decision = genre_decision.downcase.strip
+    !item[:genre].nil? && (
+      puts 'This item already has a genre'
+      return
+    )
+    if genre_decision == 'new'
       print 'Enter the genre: '
       title = gets.chomp
       add_genre(item, title)
-    elsif genre_decision.to_i.is_a? Integer
+    elsif genre_decision.to_i.is_a?(Integer)
       genre_index = genre_decision.to_i - 1
       genre = @genre_list[@menu.to_s.to_sym][genre_index][:ref]
       genre.add_item(item)
     else
-      puts ''
-      puts 'Invalid input! Try again!'
+      puts "\n\nInvalid input! Try again!"
       create_new_genre(item, genre_decision)
     end
     puts 'The genre has been added successfully!'
@@ -112,23 +117,23 @@ class CreateClasses
   end
 
   def create_new_author(item, author_decision)
+    author_decision = author_decision.downcase.strip
     !item[:author].nil? && (
       puts 'This item already has an author'
       return
     )
-    if author_decision.downcase.strip == 'new'
+    if author_decision == 'new'
       print "\n\nFirst name: "
       first_name = gets.chomp
       print 'Last name: '
       last_name = gets.chomp
       add_author(item, first_name, last_name)
-    elsif author_decision.to_i.is_a? Integer
+    elsif author_decision.to_i.is_a?(Integer)
       author_index = author_decision.to_i - 1
       author = @author_list[@menu.to_s.to_sym][author_index][:ref]
       author.add_item(item)
     else
-      puts ''
-      puts 'Invalid input! Try again!'
+      puts "\n\nInvalid input! Try again!"
       create_new_author(item, author_decision)
     end
     puts puts 'The author has been added successfully!'
