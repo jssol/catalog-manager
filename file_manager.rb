bbfb40ed41b46c2298873ba7a58cfb06eba7f7ef
Jonathan Sivahera <jsivahera@icloud.com>
2022-06-02 23:32:56 +0200

    Create file manager class

--- file_manager.rb
+++ file_manager.rb
@@ -1,19 +1,31 @@
 require 'json'
 
-def to_json(file)
-  JSON.pretty_generate(file)
-end
+class FileManager
+  def save_file(path, file)
+    File.write(path, to_json(file))
+  end
 
-def from_json(file)
-  JSON.parse(file)
-end
+  def get_file(path)
+    response = []
+    response = from_json(File.read(path)) if File.exist?(path)
+    response
+  end
 
-def save_file(path, file)
-  File.write(path, to_json(file))
-end
+  def to_hash(object)
+    hash = {}
+    object.instance_variables.each do |var|
+      hash[var.to_s.delete('@')] = object.instance_variable_get(var)
+    end
+    hash
+  end
 
-def get_file(path)
-  response = []
-  response = from_json(File.read(path)) if File.exist?(path)
-  response
+  private
+
+  def to_json(file)
+    JSON.pretty_generate(file)
+  end
+
+  def from_json(file)
+    JSON.parse(file)
+  end
 end
